66da8cfd144ec63757fd021a7bbbe1f32b0006a5
GabiLlvs <gabriellloveras@gmail.com>
2025-05-08 23:04:08 -0300

    Refactor funciones de filtrado y mapeo de productos para mejorar la legibilidad y consistencia del código

--- js/arrays.js
+++ js/arrays.js
@@ -1,52 +1,44 @@
-
 function filtrarProductosPorNombre() {
-    let valor = prompt('Ingrese el nombre de producto a filtrar:')
-    valor = valor.trim().toLowerCase()
-    
-    // Camino Feliz
-    // let productosEncontrados = productos.filter((producto)=> producto.nombre === valor )
+  let valor = prompt("Ingrese el nombre de producto a filtrar:");
+  valor = valor.trim().toLowerCase();
 
-    // Búsqueda dentro de un texto
-    // let productosEncontrados = productos.filter((producto)=> producto.nombre.includes(valor) )
+  // Camino Feliz
+  // let productosEncontrados = productos.filter((producto)=> producto.nombre === valor )
 
-    // Normalizar texto de búsqueda
-    let productosEncontrados = productos.filter((producto)=> producto.nombre.toLowerCase().includes(valor) )
-    console.log(productosEncontrados)
+  // Búsqueda dentro de un texto
+  // let productosEncontrados = productos.filter((producto)=> producto.nombre.includes(valor) )
+
+  // Normalizar texto de búsqueda
+  let productosEncontrados = productos.filter((producto) =>
+    producto.nombre.toLowerCase().includes(valor)
+  );
+  console.log(productosEncontrados);
 }
 
-/*
-    {
-        id: "1",
-        imagen: "🍌",
-        nombre: "Bananas",
-        precio: 1220,
-        categoria: "Fruta"
-    }
-*/
-
 function mapearProductos() {
-    let nuevoArray = productos.map((producto)=> {
-        return {
-            codigo: producto.id,
-            nombre: producto.nombre.toUpperCase(),
-            importe: producto.precio
-        }
-    })
-    console.log(nuevoArray)
+  let nuevoArray = productos.map((producto) => {
+    return {
+      codigo: producto.id,
+      nombre: producto.nombre.toUpperCase(),
+      importe: producto.precio,
+    };
+  });
+  console.log(nuevoArray);
 }
 
 function ordenarProductos() {
-    let veces = 0
-    productos.sort((a, b)=> { // ordenar por precio
-        veces++
-        if (a.precio > b.precio) {
-            return -1
-        }
-        if (a.precio < b.precio) {
-            return 1
-        }
-        return 0
-    })
-    console.log("Veces iteradas:", veces)
-    console.table(productos)
+  let veces = 0;
+  productos.sort((a, b) => {
+    // ordenar por precio
+    veces++;
+    if (a.precio > b.precio) {
+      return -1;
+    }
+    if (a.precio < b.precio) {
+      return 1;
+    }
+    return 0;
+  });
+  console.log("Veces iteradas:", veces);
+  console.table(productos);
 }
